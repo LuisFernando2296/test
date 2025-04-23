ca26ccfe28d03c083ba4163eead9542832768d14
Luis Fernando <loyola2296@gmail.com>
2025-04-23 15:18:00 -0600

    fix order by

--- componentes/getTareas.php
+++ componentes/getTareas.php
@@ -2,7 +2,7 @@
 require_once 'conex.php';
 
 try {
-  $stmt = $pdo->prepare("SELECT id, task_name FROM task WHERE active_flag = 1 ORDER BY created_at DESC");
+  $stmt = $pdo->prepare("SELECT id, task_name FROM task WHERE active_flag = 1 ORDER BY id DESC");
   $stmt->execute();
   $tareas = $stmt->fetchAll();
   echo json_encode($tareas);
